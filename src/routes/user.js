64411045f788dcc15e93811987592f7a43258b8a
linuxnile <linuxnile@gmail.com>
2024-11-27 01:14:05 +0530

    get all connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,4 +19,27 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
     }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedUser = req.user._id;
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedUser, status: "accepted" },
+                { toUserId: loggedUser, status: "accepted" },
+            ],
+        }).populate("fromUserId", userSafeData).populate("toUserId", userSafeData);
+
+        const data = connectionRequest.map((rows) => {
+            if (rows.fromUserId._id.equals(loggedUser)) {
+                return rows.toUserId
+            }
+            return rows.fromUserId
+        })
+
+        res.json({ message: "Data fetch successfully", data });
+    } catch (error) {
+        res.status(400).send("Error occur " + error);
+    }
+});
+
 module.exports = userRouter;
