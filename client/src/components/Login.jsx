78a6fb3b842125b81b62e1d133375d972fc55374
linuxnile <linuxnile@gmail.com>
2025-01-14 11:59:52 +0000

    Add login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmaiId] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email Id</span>
+              </div>
+              <input
+                type="email"
+                className="input input-bordered w-full max-w-xs"
+                value={emailId}
+                onChange={(e) => {
+                  setEmaiId(e.target.value);
+                }}
+              />
+            </label>
+
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="password"
+                className="input input-bordered w-full max-w-xs"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center mt-2">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
