2d73b832b74e10dca3d3395ccdb31f0b387542af
linuxnile <linuxnile@gmail.com>
2025-01-18 14:55:17 +0000

    Add photoUrl in missing places

--- server/models/user.js
+++ server/models/user.js
@@ -1,84 +1,88 @@
-const mongoose = require("mongoose")
-const validator = require("validator")
-const jwt = require("jsonwebtoken")
-const bcryptjs = require("bcryptjs")
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        minLength: 2,
-        maxLength: 20
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
     },
     lastName: {
-        type: String,
-        minLength: 2,
-        maxLength: 20
+      type: String,
+      minLength: 2,
+      maxLength: 20,
     },
     emailId: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Invalid email address")
-            }
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
         }
+      },
     },
     password: {
-        type: String,
-        required: true,
-        validate(value) {
-            if (!validator.isStrongPassword(value)) {
-                throw new Error("Passowrd is too weak")
-            }
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Passowrd is too weak");
         }
+      },
     },
     age: {
-        type: Number,
-        min: 16
+      type: Number,
+      min: 16,
     },
     gender: {
-        type: String,
-        validate(value) {
-            if (!["male", "female", "others"].includes(value)) {
-                throw new Error("Gender is invalid");
-            }
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is invalid");
         }
+      },
     },
     photoUrl: {
-        type: String,
-        validate(value) {
-            if (!validator.isURL(value)) {
-                throw new Error("Photo urls is not valid")
-            }
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo urls is not valid");
         }
+      },
     },
     about: {
-        type: String,
-        default: "Please write something in about section"
+      type: String,
+      default: "Please write something in about section",
     },
     skills: {
-        type: [String]
-    }
-},
-    { timestamps: true }
-)
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
 userSchema.methods.getJWT = async function () {
-    const user = this
-    const token = await jwt.sign({ _id: user._id }, "nillu$nillu", { expiresIn: "1d" })
-    return token
-}
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "nillu$nillu", {
+    expiresIn: "1d",
+  });
+  return token;
+};
 
 userSchema.methods.validatePassword = async function (password) {
-    const user = this
-    const passCompare = await bcryptjs.compare(password, user.password)
-    return passCompare
-}
+  const user = this;
+  const passCompare = await bcryptjs.compare(password, user.password);
+  return passCompare;
+};
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,20 +1,39 @@
-const validator = require("validator")
+const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const { firstName, lastName, emailId, password, age, photoUrl, about, skills = [], } = req.body
-    if (!validator.isEmail(emailId)) {
-        throw new Error("Please enter any valid email address")
-    } else if (!validator.isStrongPassword(password)) {
-        throw new Error("Please enter any strong password")
-    }
-}
+  const {
+    firstName,
+    lastName,
+    emailId,
+    password,
+    age,
+    photoUrl,
+    about,
+    skills = [],
+  } = req.body;
+
+  if (!validator.isEmail(emailId)) {
+    throw new Error("Please enter any valid email address");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter any strong password");
+  }
+};
 
 const validateProfileEditData = (req) => {
-    const allowedEditData = ["firstName", "lastName", "age", "gender", "about", "skills"]
-    const isUpdateAllowed = Object.keys(req.body).every((k) => {
-        return allowedEditData.includes(k)
-    })
-    return isUpdateAllowed
-}
+  const allowedEditData = [
+    "firstName",
+    "lastName",
+    "age",
+    "photoUrl",
+    "gender",
+    "about",
+    "skills",
+  ];
 
-module.exports = { validateSignUpData, validateProfileEditData }
+  const isUpdateAllowed = Object.keys(req.body).every((k) => {
+    return allowedEditData.includes(k);
+  });
+  return isUpdateAllowed;
+};
+
+module.exports = { validateSignUpData, validateProfileEditData };
